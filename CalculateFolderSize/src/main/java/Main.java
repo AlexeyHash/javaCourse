c5712193e0a5a64c6133f5ae4869e61c8ca5ff1b
Alexey <leshka1096@gmail.com>
2020-05-08 23:28:09 +1000

    CalculateSize finished

---
+++ CalculateFolderSize/src/main/java/Main.java
@@ -0,0 +1,63 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+public class Main
+{
+    private static long size;
+    private static Logger logger;
+    public static void main(String[] args)
+    {
+        File folder = new File("C:/Downloads");
+        logger = LogManager.getRootLogger();
+        try {
+            long folderSize = listFilesForFolder(folder);
+            System.out.println("\nОбщий размер папки " + folder.getName() + ": "
+                    + calculateFileSize(folderSize));
+            logger.info(calculateFileSize(folderSize));
+        }
+        catch (NullPointerException ex){
+            ex.printStackTrace();
+        }
+    }
+    private static long listFilesForFolder(File folder) {
+
+        for (final File file : folder.listFiles()) {
+
+            if (file.isDirectory()) {
+                listFilesForFolder(file);
+            }
+            else {
+                logger.info(file.getName() + " - " + calculateFileSize(file.length()));
+                System.out.println(file.getName() + " - " + calculateFileSize(file.length()));
+            }
+            size = size + file.length();
+
+        }
+        return size;
+    }
+
+    private static String calculateFileSize(long fileSize){
+        Double size = 0.0;
+        DecimalFormat format = new DecimalFormat("#0.0#");
+        if (fileSize > Math.pow(2.0, 30.0)){
+            size = fileSize / Math.pow(2.0, 30.0);
+            return format.format(size) + " Гбайт";
+        }
+        else if (fileSize > Math.pow(2.0, 20.0)){
+            size = fileSize / Math.pow(2.0, 20.0);
+            return format.format(size) + " Мбайт";
+        }
+        else if (fileSize > Math.pow(2.0, 10.0)){
+            size = fileSize / Math.pow(2.0, 10.0);
+            return format.format(size) + " Кбайт";
+        }
+        else if (fileSize < Math.pow(2.0, 10.0)){
+            size = (double) fileSize;
+            return format.format(size) + " байт";
+        }
+        return format.format(size);
+    }
+}
